3bc0f048f9bc55f27048b3107c778154b95db4c2
Wagner Pinheiro <wagner@wagnerpinheiro.com.br>
2018-03-16 16:48:51 -0300

    test with new-relic install

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 INAME="mandic/php"
-FLAVORS="5.3-fpm/garlic 5.3-fpm/oyster 5.4-fpm/coffe 5.6-fpm/peach 5.6-fpm/bacon 5.6-fpm/rice 7.0-fpm/cherry"
+FLAVORS="5.3-fpm/garlic 5.3-fpm/oyster 5.4-fpm/coffe 5.6-fpm/peach 5.6-fpm/bacon 5.6-fpm/rice 5.6-fpm/rice-nr 7.0-fpm/cherry"
 mkdir -p logs
 for F in $FLAVORS; do
 	T=${F/\//\-}
